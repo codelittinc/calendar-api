ac0a27386ea1d23565cd735659de99389b4c2635
Wellington-gc <wellington_gc@outlook.com>
2022-10-27 17:41:04 -0300

    Remove health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { getDbAppConfig } from './configs/db.config';
-import { HealthModule } from './health/health.module';
 import { TokensModule } from './tokens/tokens.module';
 import { RemindersModule } from './reminders/reminders.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(getDbAppConfig()), HealthModule, TokensModule, RemindersModule],
+  imports: [TypeOrmModule.forRoot(getDbAppConfig()), TokensModule, RemindersModule],
   controllers: [],
   providers: [],
 })
