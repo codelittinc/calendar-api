ff68a685efdce9a092849a9de546715e3b5ec90d
Wellington-gc <wellington_gc@outlook.com>
2022-10-27 18:27:50 -0300

    Improve the swagger documentation

--- src/tokens/tokens.controller.ts
+++ src/tokens/tokens.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
 import { TokensService } from './tokens.service';
 import { CreateTokenDto } from './dto/create-token.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UpdateTokenDto } from './dto/update-token.dto';
 
 @ApiTags('Tokens')
@@ -9,16 +9,37 @@ import { UpdateTokenDto } from './dto/update-token.dto';
 export class TokensController {
   constructor(private readonly tokensService: TokensService) {}
 
+  @ApiOperation({ summary: 'Create a token' })
+  @ApiResponse({
+    status: 201,
+    description: 'The token has been successfully created',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'An error has occurred when trying to create a token',
+  })
   @Post()
   async create(@Body() createTokenDto: CreateTokenDto) {
     return await this.tokensService.create(createTokenDto);
   }
 
+  @ApiOperation({ summary: 'Get a single token' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns a single token',
+  })
+  @ApiParam({ name: 'id', description: 'A valid ID of a token to get', type: 'uuid' })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.tokensService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update the data of a token' })
+  @ApiResponse({
+    status: 200,
+    description: 'The token has been updated successfully',
+  })
+  @ApiParam({ name: 'id', description: 'A valid ID of a token to update', type: 'uuid' })
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateTokenDto: UpdateTokenDto) {
     return await this.tokensService.update(id, updateTokenDto);
