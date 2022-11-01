c4272b85d44fccb7607883ae620dc1bb614fe8ba
Wellington-gc <wellington_gc@outlook.com>
2022-11-01 11:57:31 -0300

    Add a way to connect to the Database via URL

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -5,7 +5,26 @@ import { loadEnvVars } from './loadEnvVars';
 loadEnvVars();
 
 export const getDbAppConfig = () => {
-  const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
+  const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD, DATABASE_URL } = process.env;
+
+  if (DATABASE_URL) {
+    const urlParts = DATABASE_URL.replace('postgres://', '').split('@');
+    const userAndPassword = urlParts[0].split(':');
+    const hostPortAndDatabase = urlParts[1].split(':');
+    const portAndDatabase = hostPortAndDatabase[1].split('/');
+
+    return {
+      type: 'postgres',
+      username: userAndPassword[0],
+      password: userAndPassword[1],
+      host: hostPortAndDatabase[0],
+      port: +portAndDatabase[0],
+      database: portAndDatabase[1].split('?')[0],
+      autoLoadEntities: true,
+      synchronize: false,
+      migrationsRun: false,
+    } as TypeOrmModuleOptions;
+  }
 
   return {
     type: 'postgres',
@@ -21,12 +40,30 @@ export const getDbAppConfig = () => {
 };
 
 export const getDbMigrationConfig = () => {
-  const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_DOCKER_EXPOSE_PORT, DB_DOCKER_EXPOSE_HOST } = process.env;
+  const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DATABASE_URL } = process.env;
+
+  if (DATABASE_URL) {
+    const urlParts = DATABASE_URL.replace('postgres://', '').split('@');
+    const userAndPassword = urlParts[0].split(':');
+    const hostPortAndDatabase = urlParts[1].split(':');
+    const portAndDatabase = hostPortAndDatabase[1].split('/');
+
+    return {
+      type: 'postgres',
+      username: userAndPassword[0],
+      password: userAndPassword[1],
+      host: hostPortAndDatabase[0],
+      port: +portAndDatabase[0],
+      database: portAndDatabase[1].split('?')[0],
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
+    } as DataSourceOptions;
+  }
 
   return {
     type: 'postgres',
-    host: DB_DOCKER_EXPOSE_HOST,
-    port: +DB_DOCKER_EXPOSE_PORT,
+    host: DB_HOST,
+    port: +DB_PORT,
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
