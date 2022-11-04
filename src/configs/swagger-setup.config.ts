b14d007737bad48cc540203b7efca28b9fe8e727
Wellington-gc <wellington_gc@outlook.com>
2022-11-04 15:19:01 -0300

    Improve the swagger documentation description

--- src/configs/swagger-setup.config.ts
+++ src/configs/swagger-setup.config.ts
@@ -26,3 +26,39 @@ export const getSwaggerDocumentConfig = () => {
     deepScanRoutes: true,
   } as Partial<SwaggerDocumentOptions>;
 };
+
+export const getSwaggerDescription = () => {
+  return (
+    '<h2 id="using-codelitt-s-calendar-api">Using Codelitt&#39;s Calendar API</h2>' +
+    '<p>To use the Codelitt&#39;s Calendar API, follow these steps:</p>' +
+    '<ul>' +
+    '<li><p>Use the following URL to make requests through the Swagger Documentation: <a href="https://api.calendar.codelitt.dev/docs">Codelitt&#39;s Calendar API - Swagger</a>;</p>' +
+    '</li>' +
+    '<li><p>Use the following Base URL to make requests through your application:</p>' +
+    '<ul>' +
+    '<li><a href="https://api.calendar.codelitt.dev/"><code>https://api.calendar.codelitt.dev/</code></a></li>' +
+    '</ul>' +
+    '</li>' +
+    '<li><p>Create a new authentication token using the <a href="https://api.calendar.codelitt.dev/docs#/Tokens/create"><code>/tokens POST</code></a> endpoint through Swagger:</p>' +
+    '<ul>' +
+    '<li><p>The token only needs to be created once, it is a user&#39;s choice to create more tokens;</p>' +
+    '</li>' +
+    '<li><p>The <code>name</code> field on the request body is an optional field, the token can be created without it;</p>' +
+    '</li>' +
+    '<li><p>After changing the request body payload to the desired value, execute the request by clicking on the <code>Execute</code> button;</p>' +
+    '</li>' +
+    '<li><p>On the response body, the <code>id</code> field represents the authentication token ;</p>' +
+    '</li>' +
+    '<li><p>The authentication token should be stored as an Environment Variable on your application;</p>' +
+    '</li>' +
+    '<li><p>Use the authentication token on the <a href="https://www.uuidgenerator.net/version4">UUID v4</a> format to make further requests;</p>' +
+    '</li>' +
+    '</ul>' +
+    '</li>' +
+    '<li><p>Create a reminder using the <a href="https://api.calendar.codelitt.dev/docs#/Reminders/create"><code>/reminders POST</code></a> endpoint, remember to inform the authentication token on the request body;</p>' +
+    '</li>' +
+    '<li><p>Get and Manage the reminders by passing the authentication token on the URL of each Reminder method (<a href="https://api.calendar.codelitt.dev/docs#/Reminders/findRemindersNotificationsByMonth">GET</a>, <a href="https://api.calendar.codelitt.dev/docs#/Reminders/update">PATCH</a>, <a href="https://api.calendar.codelitt.dev/docs#/Reminders/remove">DELETE</a>).</p>' +
+    '</li>' +
+    '</ul>'
+  );
+};
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { loadEnvVars } from './configs/loadEnvVars';
-import { getSwaggerDocumentConfig, getSwaggerSetupConfig } from './configs/swagger-setup.config';
+import { getSwaggerDescription, getSwaggerDocumentConfig, getSwaggerSetupConfig } from './configs/swagger-setup.config';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 async function bootstrap() {
@@ -16,7 +16,7 @@ async function bootstrap() {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Calendar API Swagger Documentation - Codelitt')
     .setContact('Codelitt', 'https://www.codelitt.com/', 'hello@codelitt.com')
-    .setDescription('This is an API built to create calendars reminders.')
+    .setDescription(getSwaggerDescription())
     .setVersion('v1')
     .build();
 
