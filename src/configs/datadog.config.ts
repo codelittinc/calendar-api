bce5e19886ab7c65a8c9602c4bf403a0d1be801d
Alessandro <alessandromontividiu03@gmail.com>
2022-11-22 15:43:28 -0300

    Update datadog integration to apply default settings

--- src/configs/datadog.config.ts
+++ src/configs/datadog.config.ts
@@ -1,20 +1,9 @@
 import tracer from 'dd-trace';
 
 export const loadDatadogConfig = () => {
-  const { DATADOG_SERVICE, DATADOG_ENV, DATADOG_HOSTNAME, NODE_ENV, DD_API_KEY } = process.env;
-
+  const { DD_API_KEY } = process.env;
   if (!DD_API_KEY) {
     return;
   }
-
-  tracer.init({
-    service: DATADOG_SERVICE,
-    runtimeMetrics: true,
-    logInjection: true,
-    env: DATADOG_ENV || NODE_ENV,
-    profiling: true,
-    hostname: DATADOG_HOSTNAME,
-  });
-
-  tracer.use('express');
+  tracer.init();
 };
